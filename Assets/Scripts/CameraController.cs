64f033e9c99175e284cc6992c282398ebef5bc33
Noah Rose <nrose@uoregon.edu>
2019-04-27 21:37:47 -0700

    Made Tutorial Room with buggy movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    void LateUpdate()
+    {
+    	//Camera is bound to player position
+        transform.rotation = player.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/ItemManagement.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManagement : MonoBehaviour
+{
+	//Placeholder for interacting with pickups and activators
+}
