3dd2e5e9d95a9d866d57f4599726726b6912820e
Chase Craig <ccraig@uoregon.edu>
2019-04-30 08:37:56 -0700

    Start Scene UI nearly finished
    Just need final touches and demo room for the campaign button.

---
+++ Assets/Quit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public static string webplayerQuitURL = "http://google.com";
+    public void QuitMe()
+    {
+#if UNITY_WEBPLAYER
+    Application.OpenURL(webplayerQuitURL);  
+#elif UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+
+    }
+}
