cf9fb81347cf4fe09f50c67386325660aa185760
Chase Craig <ccraig@uoregon.edu>
2019-04-30 22:09:42 -0700

    Working on adding scene transition to Tutorial Room

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    private float delta = 0;
+    public void Update()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(0, delta, 0);
+        delta += speed * Time.deltaTime;
+    }
+}
