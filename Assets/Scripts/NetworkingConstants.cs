6497a2219fc254144421cebe9838a2c90db0a6c5
Noah Rose <nrose@uoregon.edu>
2019-05-06 20:22:43 -0700

    Added script to hold player SubIDs, made combination vault prefab
    The vault is currently not tied to any scripts so it has no animation/logic tied to it

---
+++ Assets/Scripts/NetworkingConstants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class NetworkingConstants
+{
+	public const int Player1SID = 1000;
+	public const int Player2SID = 1000;
+	public const int Player3SID = 1000;
+
+	//User send function
+
+	//User receive function
+}
