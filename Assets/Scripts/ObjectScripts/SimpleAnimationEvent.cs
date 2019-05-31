db7ff54dc03b4f36d4f790e2517c4bb3cab13371
Chase Craig <ccraig@uoregon.edu>
2019-05-30 22:42:28 -0700

    Added useful scripts
    Added simple movements events listeners and also upgraded the interact/item scripts to include a update functions (as so to update the state of the item in the middle of the puzzle [half solved, etc])

---
+++ Assets/Scripts/ObjectScripts/SimpleAnimationEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimationEvent : MonoBehaviour
+{
+    public GameLock locks;
+    public AnimationSet[] toPlay;
+
+    void Start()
+    {
+        locks.GameFinished += Locks_GameFinished;
+    }
+    private void Locks_GameFinished(CameraController cc)
+    {
+        foreach(AnimationSet ss in toPlay)
+        {
+            ss.anim.Play(ss.animationStateName);// :D
+        }
+    }
+}
+[System.Serializable]
+public class AnimationSet
+{
+    public Animator anim;
+    public string animationStateName;
+}
---
+++ Assets/Scripts/ObjectScripts/SimpleMovementEvent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovementEvent : MonoBehaviour
+{
+    public GameLock locks;
+    public MovementSet[] toPlay;
+
+    void Start()
+    {
+        locks.GameFinished += Locks_GameFinished;
+    }
+    IEnumerator smm(MovementSet ms)
+    {
+        float t = Time.time;
+        Vector3 position = ms.who.transform.position, scale =ms.who.transform.localScale;
+        Quaternion rot = ms.who.transform.rotation;
+        while(Time.time - t < ms.when)
+        {
+            yield return null;
+            ms.who.transform.position = Vector3.Lerp(position, ms.where.position, ms.how.Evaluate((Time.time - t) / ms.when));
+            ms.who.transform.localScale = Vector3.Lerp(scale, ms.where.localScale, ms.how.Evaluate((Time.time - t) / ms.when));
+            ms.who.transform.rotation = Quaternion.Slerp(rot, ms.where.rotation, ms.how.Evaluate((Time.time - t) / ms.when));
+        }
+        ms.who.transform.position = ms.where.position;
+        ms.who.transform.localScale = ms.where.localScale;
+        ms.who.transform.rotation = ms.where.rotation;
+    }
+    private void Locks_GameFinished(CameraController cc)
+    {
+        foreach (MovementSet ss in toPlay)
+        {
+            StartCoroutine(smm(ss));
+        }
+    }
+}
+[System.Serializable]
+public class MovementSet
+{
+    public GameObject who;
+    public float when;
+    public Transform where;
+    public AnimationCurve how;
+}
