97d8ebd474cb667decd0868def0ca928156be6ec
Chase Craig <ccraig@uoregon.edu>
2019-06-08 22:23:20 -0700

    Worked a lot on the tutorial room and Railway
    Not finished with railway, and inventory system is currently broken.

--- Assets/Scripts/ObjectScripts/SimpleGameDoneEvent.cs
+++ Assets/Scripts/ObjectScripts/SimpleGameDoneEvent.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class SimpleGameDoneEvent : MonoBehaviour
 {
     public GameLock locks;
-
+    public WallTimer WT;
+    public bool failGame = false;
     void Start()
     {
         locks.GameFinished += Locks_GameFinished;
     }
     private void Locks_GameFinished(CameraController cc, int _)
     {
+        if (failGame)
+        {
+            // >:D
+            WT.TimeToCountTo = Time.time;
+            return;
+        }
         if (RoomManager.instance.CMMP.nm.net.isHost)
         {
             RoomManager.instance.roomTimer.CmdStopTimer();
