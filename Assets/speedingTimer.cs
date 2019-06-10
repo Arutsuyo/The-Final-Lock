72e348f31bae10b1b681cb77426645e825acff8d
Chase Craig <ccraig@uoregon.edu>
2019-06-10 09:32:41 -0700

    Railway completed

---
+++ Assets/speedingTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedingTimer : MonoBehaviour
+{
+    public GameObject toMove;
+    public GameObject from;
+    public GameObject to;
+
+    public float startTime = -1;
+    public WallTimer wt;
+    public AnimationCurve ac;
+    public void Start()
+    {
+        wt.TimerTicked += Wt_TimerTicked;
+    }
+
+    private void Wt_TimerTicked(float timeRem)
+    {
+        if(startTime < 0)
+        {
+            startTime = timeRem;
+        }
+        toMove.transform.position = Vector3.Lerp(to.transform.position, from.transform.position, ac.Evaluate(timeRem / startTime));
+    }
+}
